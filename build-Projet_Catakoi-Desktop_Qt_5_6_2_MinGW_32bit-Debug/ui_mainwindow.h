ea2108481f3cd89d5004bde1d522388c32988eb2
Aurelien-Valeille <aurelien.valeille@sfr.fr>
2017-04-12 16:35:07 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Projet_Catakoi/Projet_Catakoi.pro.user
    #	Projet_Catakoi/mainwindow.cpp
    #	Projet_Catakoi/mainwindow.ui

---
+++ build-Projet_Catakoi-Desktop_Qt_5_6_2_MinGW_32bit-Debug/ui_mainwindow.h
@@ -0,0 +1,139 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.6.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSlider>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+#include <myglwidget.h>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QGridLayout *gridLayout_3;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout;
+    QVBoxLayout *verticalLayout_3;
+    MyGLWidget *myGLWidget;
+    QLabel *camFrame;
+    QSlider *zoomSlider;
+    QPushButton *pushButton;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(701, 427);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        centralWidget->setEnabled(true);
+        gridLayout_3 = new QGridLayout(centralWidget);
+        gridLayout_3->setSpacing(6);
+        gridLayout_3->setContentsMargins(11, 11, 11, 11);
+        gridLayout_3->setObjectName(QStringLiteral("gridLayout_3"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setSizeConstraint(QLayout::SetDefaultConstraint);
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setSpacing(6);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setSizeConstraint(QLayout::SetDefaultConstraint);
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setSpacing(6);
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        myGLWidget = new MyGLWidget(centralWidget);
+        myGLWidget->setObjectName(QStringLiteral("myGLWidget"));
+        camFrame = new QLabel(myGLWidget);
+        camFrame->setObjectName(QStringLiteral("camFrame"));
+        camFrame->setGeometry(QRect(0, 0, 141, 121));
+        camFrame->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_3->addWidget(myGLWidget);
+
+        zoomSlider = new QSlider(centralWidget);
+        zoomSlider->setObjectName(QStringLiteral("zoomSlider"));
+        zoomSlider->setMinimum(1);
+        zoomSlider->setMaximum(100);
+        zoomSlider->setValue(100);
+        zoomSlider->setOrientation(Qt::Horizontal);
+        zoomSlider->setTickPosition(QSlider::TicksBelow);
+        zoomSlider->setTickInterval(1);
+
+        verticalLayout_3->addWidget(zoomSlider);
+
+
+        horizontalLayout->addLayout(verticalLayout_3);
+
+        horizontalLayout->setStretch(1, 5);
+
+        gridLayout_3->addLayout(horizontalLayout, 0, 0, 1, 1);
+
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        gridLayout_3->addWidget(pushButton, 1, 0, 1, 1);
+
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 701, 21));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+        QObject::connect(zoomSlider, SIGNAL(valueChanged(int)), myGLWidget, SLOT(setZoom(int)));
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        camFrame->setText(QApplication::translate("MainWindow", "Image", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "Start Tracking", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
